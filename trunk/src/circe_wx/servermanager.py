ac2ec972bd094e586c1ba49fe2dd79e5b4339124
anjel <anjel@8a99981f-f2d9-0310-b0a6-bd6f12ac4734>
2004-10-08 00:06:31 +0000

    Added the license statement to servermanager.py, also cleaned up TextCommand just a little.

--- trunk/src/circe_wx/servermanager.py
+++ trunk/src/circe_wx/servermanager.py
@@ -1,3 +1,20 @@
+# Circe
+# Copyright (C) 2004 The Circe development team
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
 from circelib.circelib import Server
 
 servers = []
@@ -17,6 +34,8 @@ def TextCommand(s,cmdstring):
     # Strip /
     if cmdstring[0] == "/":
         cmdstring = cmdstring[1:]
+    else:
+        return
     # Create a list
     cmdlist = cmdstring.split()
     cmd = cmdlist[0]
@@ -29,6 +48,8 @@ def TextCommand(s,cmdstring):
         s.nick(*params)
     elif(cmd == "msg"):
         channel = params[0]
-        text = params[1:];text=" ".join(text); s.sendMessage(channel, text)
+        text = params[1:]
+        text=" ".join(text)
+        s.sendMessage(channel, text)
     elif(cmd == "quit"):
         s.closeConnection()        

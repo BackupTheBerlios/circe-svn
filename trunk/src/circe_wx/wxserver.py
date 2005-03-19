5954c78fa9bd037794a9f1b4ac135122318d0fad
alst <alst@8a99981f-f2d9-0310-b0a6-bd6f12ac4734>
2005-03-19 18:04:55 +0000

    Grouped CirceIRCClient() with WXServer() in wxserver.py. And adding a new method to check for events in CirceIRCClient(). circelib is now useless.

--- trunk/src/circe_wx/wxserver.py
+++ trunk/src/circe_wx/wxserver.py
@@ -1,12 +1,41 @@
 from window_status import window_status
 from window_channel import window_channel
-from circelib.circelib import CirceIRCClient
+import irclib
 
 
+class CirceIRCClient(irclib.SimpleIRCClient):
+    def __init__(self):
+        irclib.SimpleIRCClient.__init__(self)
+        # Here are stored Events objects to be processed (use Event's methods
+        # eventtype(), source(), target(), arguments() to have all info about
+        # the event)
+        self.new_events = []
+
+    def setDebug(self, flag):
+        """Turn on/off the debug mode."""
+        irclib.DEBUG = flag
+        print "Debug mode", flag and "on" or "off"
+
+    def checkEvents(self):
+        """Check for new events, then return them in a list or return an empty
+        list if there is no new event.
+        """
+        self.new_events = []
+        self.ircobj.process_once()
+        return self.new_events
+
+    def on_welcome(self, c, e):
+        self.new_events.append(e)
+
+    def on_privmsg(self, c, e):
+        self.new_events.append(e)
+    
+    
 class WXServer(CirceIRCClient):
-    def __init__(self,windowarea, target=""):
-        CirceIRCClient.__init__(self, target)
-        self.host = self.target
+    def __init__(self,windowarea):
+        CirceIRCClient.__init__(self)
+        self.c = self.connection    # just because it's shorter
+        self.host = self.c.connected and self.c.get_server_name() or ""
         self.statuswindow = window_status(windowarea,self)
         windowarea.AddWindow(self.statuswindow)
         self.windowarea = windowarea
@@ -36,6 +65,18 @@ class WXServer(CirceIRCClient):
             port = 6667
             nick = params[1]
             self.connection.connect(server=server, port=port, nickname=nick)
+            self.host = server
+            print "Server: %s %s" % (server,
+                    self.connection.get_server_name())
+        elif cmd =="check": # for testing
+            print "-"*10, "CheckEvents"
+            for e in self.checkEvents():
+                print "--\nEvent: %s Source: %s Target: %s\nArguments: %s" % ( 
+                        e.eventtype(), e.source(), e.target(), e.arguments())
+        elif cmd == "debug":
+            self.setDebug(True)
+        elif cmd == "nodebug":
+            self.setDebug(False)
         elif cmd == "join":
             self.connection.join(*params)
         elif cmd == "nick":

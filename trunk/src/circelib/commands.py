0dd842c8f6633f4bbecdf142f7d2b3e5d67ea5af
kab <kab@8a99981f-f2d9-0310-b0a6-bd6f12ac4734>
2005-10-11 20:47:02 +0000

    removed circelib.{parser,commands}

--- trunk/src/circelib/commands.py
+++
@@ -1,7 +0,0 @@
-COMMANDS = {} # TODO: remove this global
-
-class Command(object):
-    def __init__(self, name):
-        self.name = name
-        COMMANDS[name] = self.execute
-    def execute(self, event, window, cmd, *args): pass # override

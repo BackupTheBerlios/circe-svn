7b7d6ba38ae58caeeb11f25ab1238941932a087a
nick125 <nick125@8a99981f-f2d9-0310-b0a6-bd6f12ac4734>
2005-10-15 05:18:11 +0000

    Added version info in the Other tab in the about panel

--- trunk/src/circe_wx/frame_main.py
+++ trunk/src/circe_wx/frame_main.py
@@ -192,11 +192,11 @@ class About(wxPython.wx.wxDialog):
 
         self.notebook = wx.Notebook(self, -1, size=(100,110))
         self.main_panel = wx.Panel(self.notebook)
-        self.authors_panel = wx.Panel(self.notebook)
+        self.credits_panel = wx.Panel(self.notebook)
         self.other_panel = wx.Panel(self.notebook)
 
         self.notebook.AddPage(self.main_panel, "About "+circe_globals.APPNAME)
-        self.notebook.AddPage(self.authors_panel, "Credits")
+        self.notebook.AddPage(self.credits_panel, "Credits")
         self.notebook.AddPage(self.other_panel, "Other")
 
         self.dialog_sizer = wx.BoxSizer(wx.VERTICAL)
@@ -210,22 +210,34 @@ class About(wxPython.wx.wxDialog):
         self.main_panel.SetSizer(self.main_sizer)
 
         self.credits_sizer = wx.BoxSizer(wx.VERTICAL)
-        self.authors_panel.SetSizer(self.credits_sizer)
+        self.credits_panel.SetSizer(self.credits_sizer)
+
+        self.other_sizer = wx.BoxSizer(wx.VERTICAL)
+        self.other_panel.SetSizer(self.other_sizer)
 
         self.about_text = "%s %s\nPython IRC Client\n%s\n" % (circe_globals.APPNAME, circe_globals.VERSION, circe_globals.HOMEPAGE)
         self.about_text_dlg = wx.TextCtrl(self.main_panel, -1,"",wx.DefaultPosition,(60,60),wx.TE_MULTILINE|wx.TE_CENTRE)
         self.about_text_dlg.AppendText(self.about_text)
         self.about_text_dlg.SetEditable(0)
+
         self.bitmap = wx.StaticBitmap(self.main_panel, -1)
         self.bitmap.SetBitmap(wx.BitmapFromImage(wx.Image(os.path.join("images", "circe.png"), wx.BITMAP_TYPE_PNG)))
+
         self.main_sizer.Add(self.bitmap,1,wx.CENTER)
         self.main_sizer.Add(self.about_text_dlg, 2, wx.LEFT|wx.EXPAND|wx.ALL)
 
         self.credits_text = open("doc/CREDITS.txt").read()
-        self.credits_text_dlg = wx.TextCtrl(self.authors_panel, -1,"",wx.DefaultPosition,wx.DefaultSize,wx.TE_MULTILINE|wx.TE_CENTRE)
+        self.credits_text_dlg = wx.TextCtrl(self.credits_panel, -1,"",wx.DefaultPosition,wx.DefaultSize,wx.TE_MULTILINE|wx.TE_CENTRE)
         self.credits_text_dlg.AppendText(self.credits_text)
         self.credits_text_dlg.SetEditable(0)
         self.credits_sizer.Add(self.credits_text_dlg, 1, wx.CENTER|wx.EXPAND|wx.ALL)
+      
+        python_version = "%s.%s.%s %s" % (sys.version_info[0], sys.version_info[1], sys.version_info[2], sys.version_info[3])
+        self.other_text = "%s Version: %s\nWxPython Version: %s\npython-irclib Version: %s\n" % (circe_globals.APPNAME, circe_globals.VERSION, python_version, "1.4.2")
+        self.other_text_dlg = wx.TextCtrl(self.other_panel, -1,"",wx.DefaultPosition,wx.DefaultSize,wx.TE_MULTILINE|wx.TE_CENTRE)
+        self.other_text_dlg.AppendText(self.other_text)
+        self.other_text_dlg.SetEditable(0)
+        self.other_sizer.Add(self.other_text_dlg, 1, wx.CENTER|wx.EXPAND|wx.ALL)
 
         wxPython.wx.EVT_BUTTON(self,100,self.OnClick)        
         

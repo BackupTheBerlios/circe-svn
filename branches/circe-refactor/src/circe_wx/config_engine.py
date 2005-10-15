948652cfdf3446b5960a85be13ad51089de044a2
kab <kab@8a99981f-f2d9-0310-b0a6-bd6f12ac4734>
2005-10-15 12:43:24 +0000

    Creating Circe refactor branch (to refactor wxserver.py)

---
+++ branches/circe-refactor/src/circe_wx/config_engine.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+# Circe
+# Copyright (C) 2004-2005 The Circe development team
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+# Some imports...here we go!
+import ConfigParser # we need some way to work with those damn INI config files ;)
+import circe_config
+import os
+
+class Config(object):
+    def __init__(self, configsection="Circe", configfile="~/.circe/config"):
+        self.config = ConfigParser.ConfigParser()
+        self.configfile = os.path.expanduser(configfile)
+        self.config.readfp(open(self.configfile, "r"))
+        self.section = configsection
+
+    def __getitem__(self, k):
+        result = self.__dict__.get("initialized", False)
+        if not result:
+            if self.__dict__.has_key(k):
+                return self.__dict__[k]
+        try:
+            return self.config.get(self.section, k)
+        except ConfigParser.NoSectionError:
+            raise KeyError, k
+    def __setitem__(self, k, v):
+        self.config.set(self.section, k, v)
+        self.__dict__[k] = v
+        self.config.write(open(self.configfile, "w"))
+
+    def __delitem__(self, k):
+        self.config.remove_option(self.section, k)
+        self.config.write(open(self.configfile, "w"))
+
+    def __getattr__(self, k):
+        try: return object.__getattr__(self, k)
+        except AttributeError: pass
+        try:
+            return getattr(circe_config, k)
+        except AttributeError: pass
+        try: return self[k]
+        except KeyError: pass
+        return getattr(self.config, k)
+
+    def getboolean(self, k):
+        try:
+            return self.config.getboolean(self.section, k)
+        except ConfigParser.NoOptionError:
+            return False
---
+++ branches/circe-refactor/src/release.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+import shutil, os, tarfile, sys, ftplib, cStringIO as StringIO
+
+def export(src, dest):
+    shutil.copytree(src, dest)
+    
+def prompt():
+    rn = raw_input("Name of release: ")
+    vn = raw_input("Version: ")
+    return [rn, vn]
+rn, vn = prompt()
+
+name = "%s-%s" % (rn, vn)
+path = "%s/%s" % (os.environ["HOME"], name)
+tfpath = "%s.tar.gz" % path
+
+print "Are you sure you want to make release %s? [y/N]" % name,
+
+answer = raw_input()
+answer = answer.lower()
+if not answer or answer[0] != "y":
+    print "Aborting!"
+    sys.exit()
+if os.path.exists(path):
+    print "=> Cleaning out %s" % path
+    shutil.rmtree(path)
+print "=> Exporting %s to %s" % (os.getcwd(), path)
+export(os.getcwd(), path)
+os.chdir(path)
+os.unlink('release.py')
+os.chdir(os.environ["HOME"])
+os.system('bash')
+tf = tarfile.open(tfpath, 'w:gz')
+tf.add(path)
+tf.close()
+

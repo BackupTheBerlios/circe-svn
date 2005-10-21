37882475342dd053ed2cf0b713a3e8848728f880
nick125 <nick125@8a99981f-f2d9-0310-b0a6-bd6f12ac4734>
2005-10-21 00:22:00 +0000

    cleaned up voodoo code down to: txt = " ".join(self.txt_edit.GetValue().split(" ")[:len(self.txt_edit.GetValue().split(" "))-1])

--- trunk/src/circe_wx/window_base.py
+++ trunk/src/circe_wx/window_base.py
@@ -134,8 +134,7 @@ class WindowTextEdit(WindowServer):
                         self.txt_edit.SetInsertionPointEnd()
                         break
                     else:
-                        txt = self.txt_edit.GetValue().split(" ")
-                        txt = " ".join(txt[:len(txt)-1])
+                        txt = " ".join(self.txt_edit.GetValue().split(" ")[:len(self.txt_edit.GetValue().split(" "))-1])
                         self.txt_edit.SetValue(txt+" "+user+" ")
                         self.txt_edit.SetInsertionPointEnd()
                         break

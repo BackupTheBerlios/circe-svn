7462df20055ddb9a9cc93132210628431a286626
nick125 <nick125@8a99981f-f2d9-0310-b0a6-bd6f12ac4734>
2005-10-22 02:00:44 +0000

    fixed setup.py

--- trunk/src/setup.py
+++ trunk/src/setup.py
@@ -4,10 +4,10 @@ from distutils.core import setup
 
 setup(
     name="circe",
-    version="0.0.3a4",
+    version="0.0.3a5",
     url="http://circe.berlios.de",
     description="A GUI-based IRC Client written in Python",
     license="GPL",
     packages=['circelib', 'circe_wx'],
-    scripts=['bin/circe.py']
+    scripts=['circe.py']
 )
